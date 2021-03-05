81da41416480f3d65d33704d16cd95af3db49952
RahamRaf <20357439+RahamRaf@users.noreply.github.com>
2021-03-05 10:57:55 +0100

    Initiate Chartjs.js file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
   <title>Fonky Sales Raport</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.min.js" integrity="sha512-SuxO9djzjML6b9w9/I07IWnLnQhgyYVSpHZx0JV97kGBfTIsUYlWflyuW4ypnvhBrslz1yJ3R+S14fdCWmSmSA==" crossorigin="anonymous"></script>
 </head>
 <body>
 
