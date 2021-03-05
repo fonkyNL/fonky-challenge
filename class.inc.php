7205d59529a65f8e9a0a21b93e8f00c8727b1788
RahamRaf <20357439+RahamRaf@users.noreply.github.com>
2021-03-05 15:09:20 +0100

    Insert data from csv file once

--- class.inc.php
+++ class.inc.php
@@ -6,22 +6,41 @@
  * @return array of data
  */
 function transform_csv() {
+  // Get the database
+  $db = new FonkyDB();
 
-  // Open the csv file
-  if (($handle = fopen( "orders.csv" , "r" )) !== FALSE) {
+  // Test to see if the data is not already inserted
+  $result = $db->select( "fonky_sales_data_status", array( "status") );
+  if( $result[0] < 1 ) {
+
+    // Open the csv file
+    if (($handle = fopen( "orders.csv" , "r" )) !== FALSE) {
       //read first row (titles)
       $titles = read_row( $handle );
 
       //read the csv file
       while ( ( $data = read_row( $handle ) ) !== FALSE) {
         //assign keys
-        $rows[] = array_combine( $titles, $data );
+        // This is not needed any more. Just keeping for in case
+        $row[] = array_combine( $titles, $data );
+
+        //formating date
+        $date = DateTime::createFromFormat('d/m/Y H:i', $data[2]);
+
+        $db->insert( "fonky_sales_data", array( 'id', 'koper', 'datum', 'product', 'vestiging', 'verkoper' ),
+                                         array( "$data[0]", "\"$data[1]\"", "\"". $date->format("Y-m-d H:i:s") ."\"", "\"$data[3]\"", "\"$data[4]\"", "\"$data[5]\"" ) );
+
       }
 
-    fclose($handle);
+      fclose($handle);
+
+      //set the status so the program insert data only once
+      $db->insert( "fonky_sales_data_status", array( "status", "datum" ), array( "1", '\''. date("Y-m-d H:i:s") .'\'' ) );
+    }
+
   }
 
-  return $rows;
+  //return $rows;
 }
 
 /**
--- database.inc.php
+++ database.inc.php
@@ -6,7 +6,73 @@
 
 class FonkyDB {
 
-  
+  private $host = "localhost";
+  private $username = "fonky";
+  private $password = "password";
+  private $database = "fonky";
+
+  private $conn;
+
+  function __construct() {
+
+    // Database connection
+    $this->conn = new mysqli( $this->host , $this->username , $this->password , $this->database);
+
+    //Initiate data table if not exists
+    $query = "CREATE TABLE IF NOT EXISTS fonky_sales_data(
+      id INT,
+      koper VARCHAR(50),
+      datum DateTime,
+      product VARCHAR(10),
+      vestiging VARCHAR(20),
+      verkoper VARCHAR(50)
+    ) ENGINE = INNODB;";
+    $result = $this->conn->query( $query );
+
+    // Initiate status table if not exists
+    $query = "CREATE TABLE IF NOT EXISTS fonky_sales_data_status(
+      status TINYINT(1),
+      datum DateTime
+    ) ENGINE = INNODB;";
+    $result = $this->conn->query( $query );
+
+    //mysqli_free_result($result);
+  }
+
+  /**
+   * Insert data into db
+   * @param string table name
+   * @param array $fileds that data hould be insert into
+   * @param array $values that should be inserted
+   */
+  function insert( $table, $fields, $values ) {
+
+    $query = "INSERT INTO $table ( ". implode( ", ", $fields ) ." ) VALUES ( ". implode( ",", $values) ." );";
+    echo $query . "<br>";
+    $result = $this->conn->query( $query );
+    echo $this->conn->error;
+    return $result;
+
+  }
+
+  /**
+   * selects data from db
+   * @param string table name
+   * @param array $fileds that data hould be insert into
+   * @param array $options that should be inserted
+   */
+  function select( $table, $fields = array("*"), $options = "" ) {
+
+    $query = "SELECT ". implode( ", ", $fields ) ." FROM $table $options;";
+    $result = $this->conn->query( $query );
+
+    return array( $result->num_rows , $result->fetch_array() );
+
+  }
+
+  function __deconstruct() {
+    $this->conn->close();
+  }
 
 }
 
