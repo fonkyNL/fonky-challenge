49ce2f14874c98f1f991a85410f5125a63e1fe54
umarssalem <umarssalem@gmail.com>
2023-06-11 23:10:15 +0200

    added index and edit functions to OrdersController

--- dashboard/app/Http/Controllers/OrdersController.php
+++ dashboard/app/Http/Controllers/OrdersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Order;
+use App\Tables\OrdersTable;
 
 
 class OrdersController extends Controller
@@ -12,7 +14,17 @@ class OrdersController extends Controller
 
     public function index(): View
     {
+        $table = new OrdersTable();
         return view('Orders.index');
     }
+    
+    public function edit(): View
+    {
+        dd('edit');
+        // $orders = Order::all();
+        return view('Orders.index');
+    }
+
+
     
 }
