4ba4e4260348db7e1ab0810448f0eaf451b0b73c
umarssalem <umarssalem@gmail.com>
2023-06-13 01:42:03 +0200

    removed debug comments and changed the version of fontAwesome

--- dashboard/resources/views/Orders/index.blade.php
+++ dashboard/resources/views/Orders/index.blade.php
@@ -1,25 +1,15 @@
+<head>
 @livewireStyles
 @livewireScripts
-
-<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css" />  
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
-<!-- 
-<div class="card">
-  <div class="card-header">
-    Orders
-  </div>
-  <div class="card-body">
-    <blockquote class="blockquote mb-0"> -->
-		  <livewire:table :config="App\Tables\OrdersTable::class"/>
-
-    <!-- </blockquote>
-  </div>
-</div> -->
+  <body> 
+    <livewire:table :config="App\Tables\OrdersTable::class"/>
+  </body>
