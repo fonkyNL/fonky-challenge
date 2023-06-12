8d402cced2b2be0eff61fe84018a1bff5c518d14
umarssalem <umarssalem@gmail.com>
2023-06-12 04:14:27 +0200

    added font awesome

--- dashboard/public/js/app.js
+++ dashboard/public/js/app.js
@@ -3021,6 +3021,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 
+// Listen to the action confirmation request
+Livewire.on('laraveltable:action:confirm', function (actionType, actionIdentifier, modelPrimary, confirmationQuestion) {
+  // You can replace this native JS confirm dialog by your favorite modal/alert/toast library implementation. Or keep it this way!
+  if (window.confirm(confirmationQuestion)) {
+    // As explained above, just send back the 3 first argument from the `table:action:confirm` event when the action is confirmed
+    Livewire.emit('laraveltable:action:confirmed', actionType, actionIdentifier, modelPrimary);
+  }
+});
+Livewire.on('laraveltable:action:feedback', function (feedbackMessage) {
+  // Replace this native JS alert by your favorite modal/alert/toast library implementation. Or keep it this way!
+  window.alert(feedbackMessage);
+});
+Livewire.on('laraveltable:link:open:newtab', function (url) {
+  window.open(url, '_blank').focus();
+});
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
--- dashboard/resources/views/Orders/index.blade.php
+++ dashboard/resources/views/Orders/index.blade.php
@@ -1,13 +1,16 @@
-
+@livewireStyles
 @livewireScripts
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />  
     <script src="{{ asset('js/app.js') }}" defer></script>
-
 </head>
+
 <!-- <button type="button" class="btn btn-primary">Primary</button>
 <button type="button" class="btn btn-secondary">Secondary</button>
 <button type="button" class="btn btn-success">Success</button>
@@ -16,14 +19,15 @@
 <button type="button" class="btn btn-info">Info</button>
 <button type="button" class="btn btn-light">Light</button>
 <button type="button" class="btn btn-dark">Dark</button> -->
-
+<!-- 
 <div class="card">
   <div class="card-header">
     Orders
   </div>
   <div class="card-body">
-    <blockquote class="blockquote mb-0">
+    <blockquote class="blockquote mb-0"> -->
 		  <livewire:table :config="App\Tables\OrdersTable::class"/>
-    </blockquote>
+
+    <!-- </blockquote>
   </div>
-</div>
+</div> -->
