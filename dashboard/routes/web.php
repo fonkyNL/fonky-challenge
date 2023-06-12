1e908b2beca0637854fab2201a924e662f14db7d
umarssalem <umarssalem@gmail.com>
2023-06-12 16:12:36 +0200

    added routes for orders

--- dashboard/routes/web.php
+++ dashboard/routes/web.php
@@ -20,6 +20,27 @@ Route::get('/', function () {
 
 
 //orders
-Route::resource('/orders', OrdersController::class);
+// Route::resource('orders', OrdersController::class);
+
+// Index route
+Route::get('/orders', [OrdersController::class, 'index'])->name('orders.index');
+
+// Show route
+Route::get('/orders/{order}', [OrdersController::class, 'show'])->name('orders.show');
+
+// Create route
+Route::get('/orders/create', [OrdersController::class, 'create'])->name('orders.create');
+
+// Store route
+// Route::post('/orders', [OrdersController::class, 'store'])->name('orders.store');
+
+// Edit route
+Route::get('/orders/{order}/edit', [OrdersController::class, 'edit'])->name('orders.edit');
+
+// Update route
+Route::get('/orders/{order}/update', [OrdersController::class, 'update'])->name('orders.update');
+
+// Delete route
+Route::delete('/orders/{order}/delete', [OrdersController::class, 'destroy'])->name('orders.destroy');
 
 
