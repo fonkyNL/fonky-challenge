d26891bbe96a9dc25e075a417c1eb896bc0a1f84
umarssalem <umarssalem@gmail.com>
2023-06-12 04:14:48 +0200

    added row actions

--- dashboard/app/Tables/OrdersTable.php
+++ dashboard/app/Tables/OrdersTable.php
@@ -5,7 +5,10 @@ namespace App\Tables;
 use App\Models\Order;
 use Okipa\LaravelTable\Table;
 use Okipa\LaravelTable\Column;
-
+use Okipa\LaravelTable\RowActions\EditRowAction;
+use Okipa\LaravelTable\RowActions\ShowRowAction;
+use Okipa\LaravelTable\RowActions\DestroyRowAction;
+use Okipa\LaravelTable\HeadActions\CreateHeadAction;
 use Illuminate\Database\Eloquent\Builder;
 use Okipa\LaravelTable\Abstracts\AbstractTableConfiguration;
 
@@ -14,18 +17,36 @@ class OrdersTable extends AbstractTableConfiguration
     protected function table(): Table
     {
         return Table::make()
-            ->model(Order::class);
-    }  
+            ->model(Order::class)
+            ->headAction(new CreateHeadAction(route('orders.create')))
+            ->rowActions(fn(Order $order) => [
+                new ShowRowAction(route('orders.show', $order)),
+                new EditRowAction(route('orders.edit', $order)),
+                new DestroyRowAction(route('orders.destroy', $order))
+            ]);
+    }
 
     protected function columns(): array
     {
         return [
-            Column::make('id')->sortable(),
-            Column::make('koper')->searchable()->sortable(),
-            Column::make('created_at')->sortable(),
-            Column::make('product'),
-            Column::make('vestiging'),
-            Column::make('verkoper'),
+            Column::make('id')
+                ->title('ID')
+                ->sortable(),
+            Column::make('koper')
+                ->title('koper')
+                ->searchable()
+                ->sortable(),
+            Column::make('product')
+                ->title('product')
+                ->searchable(),
+            Column::make('vestiging')
+                ->title('vestiging'),
+            Column::make('verkoper')
+                ->title('verkoper'),
+            Column::make('created_at')
+                ->title('created_at')
+                ->sortable()
         ];
     }
+    
 }
