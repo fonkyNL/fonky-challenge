08287fec12d0a3295eabef3733fd09a24fb88b05
umarssalem <umarssalem@gmail.com>
2023-06-12 02:00:34 +0200

    fixed webPack to resolve css correctly

--- dashboard/webpack.mix.js
+++ dashboard/webpack.mix.js
@@ -1,7 +1,9 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
-    .vue();
+    .sass('resources/sass/app.scss', 'public/css')
+    .extract(['bootstrap'])
+    .styles([
+        'resources/css/app.css',
+    ], 'public/css/app.css')
+    .version();
