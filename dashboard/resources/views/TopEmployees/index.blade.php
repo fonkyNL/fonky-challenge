55a3e8659d5161fa3905cc1d01af15ccbf7b0d88
umarssalem <umarssalem@gmail.com>
2023-06-13 05:57:29 +0200

    created Topten blades

--- dashboard/resources/views/TopEmployees/index.blade.php
+++ dashboard/resources/views/TopEmployees/index.blade.php
@@ -1,1 +1,17 @@
 @extends('layouts.master')
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Employee</th>
+      <th scope="col">Number of sells</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($employees as $name => $number)
+    <tr>
+      <td>{{ $name }}</td>
+      <td>{{ $number }}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
--- dashboard/resources/views/TopSellers/index.blade.php
+++ dashboard/resources/views/TopSellers/index.blade.php
@@ -1,1 +1,17 @@
 @extends('layouts.master')
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Product</th>
+      <th scope="col">Number of sells</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($products as $name => $number)
+    <tr>
+      <td>{{ $name }}</td>
+      <td>{{ $number }}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
