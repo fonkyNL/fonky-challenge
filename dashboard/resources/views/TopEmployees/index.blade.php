d7cae92c936fa8b72c8b6bb5c6a96781728c31a3
umarssalem <umarssalem@gmail.com>
2023-06-13 06:42:06 +0200

    added a show button  for associated orders in products and employees blade

--- dashboard/resources/views/TopEmployees/index.blade.php
+++ dashboard/resources/views/TopEmployees/index.blade.php
@@ -4,6 +4,7 @@
     <tr>
       <th scope="col">Employee</th>
       <th scope="col">Number of sells</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -11,6 +12,9 @@
     <tr>
       <td>{{ $name }}</td>
       <td>{{ $number }}</td>
+      <td>
+        <a href="{{ route('orders.show-employee-orders' , ['employee' => $name]) }}" class="fa-solid fa-eye fa-fw"></a>  
+      </td>
     </tr>
     @endforeach
   </tbody>
--- dashboard/resources/views/TopSellers/index.blade.php
+++ dashboard/resources/views/TopSellers/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.master')
+
 <table class="table">
   <thead>
     <tr>
       <th scope="col">Product</th>
       <th scope="col">Number of sells</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -11,6 +13,9 @@
     <tr>
       <td>{{ $name }}</td>
       <td>{{ $number }}</td>
+      <td>
+      <a href="{{ route('orders.show-product-orders', ['product' => $name]) }}" class="fa-solid fa-eye fa-fw"></a>
+      </td>
     </tr>
     @endforeach
   </tbody>
