e2b1e07800cdda4b4f34e50635638ffb1c4ebc8a
umarssalem <umarssalem@gmail.com>
2023-06-13 05:14:01 +0200

    fixed the routes

--- dashboard/routes/web.php
+++ dashboard/routes/web.php
@@ -4,30 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\HomeController;
 
+
+
+//Home Routes
 Route::get('/' , [HomeController::class, 'index']);
+Route::get('best-sellers', [HomeController::class, 'showProducts'])->name('show-products');
+Route::get('best-employees', [HomeController::class, 'showEmployees'])->name('show-employees');
 
-// Index route
+///Orders routes
 Route::get('orders', [OrdersController::class, 'index'])->name('orders.index');
-
-
-
-
-// Create route
 Route::get('orders/create', [OrdersController::class, 'create'])->name('orders.create');
-
-// Store route
 Route::get('orders/store', [OrdersController::class, 'store'])->name('orders.store');
-
-
-// Edit route
 Route::get('orders/{order}/edit', [OrdersController::class, 'edit'])->name('orders.edit');
-
-// Update route
 Route::get('orders/{order}/update', [OrdersController::class, 'update'])->name('orders.update');
-
-
-// Delete route
 Route::get('orders/destroy/{order}', [OrdersController::class, 'destroy'])->name('orders.destroy');
-
-// Show route
 Route::get('orders/{order}', [OrdersController::class, 'show'])->name('orders.show');
